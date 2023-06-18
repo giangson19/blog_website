820ea4462ac0c14ab9e4e3f401884557b788820b
giangson19 <giangson.lumos@gmail.com>
2023-06-18 22:34:00 +0700

    Add description field to model Post

--- blog/models.py
+++ blog/models.py
@@ -10,7 +10,7 @@ STATUS = (
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    # description = models.CharField(max_length = 300)
+    description = models.CharField(max_length = 300)
     author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_posts', default='giangson')
     updated_on = models.DateTimeField(auto_now= True)
     # content = models.TextField()
