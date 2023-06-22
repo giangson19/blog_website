2b090d53753b059430949397470a6ed1bef0b085
giangson19 <giangson.lumos@gmail.com>
2023-06-22 23:39:40 +0700

    Remove comments

--- blog/models.py
+++ blog/models.py
@@ -13,7 +13,6 @@ class Post(models.Model):
     description = models.CharField(max_length = 300)
     author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_posts', default='giangson')
     updated_on = models.DateTimeField(auto_now= True)
-    # content = models.TextField()
     content = HTMLField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
