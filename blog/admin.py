2cf6579e460bddc0db0ff4a2baee0efeb9868de7
giangson19 <giangson.lumos@gmail.com>
2023-09-01 21:47:06 +0700

    Add comment functionalities

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post, About
+from .models import Post, About, Comment
 
 # admin.site.register(Post)
 
@@ -9,6 +9,11 @@ class PostAdmin(admin.ModelAdmin):
     search_fields = ['title', 'content']
     prepopulated_fields = {'slug': ('title',)}
 
-
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'post', 'created', 'active']
+    list_filter = ['active', 'created', 'updated']
+    search_fields = ['name', 'email', 'body']
+    
 admin.site.register(Post, PostAdmin)
 admin.site.register(About)
