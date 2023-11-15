b078f3ff3b5b546dfa0eff4557dbd10af825bb92
Giang Son <son.nguyen15@onemount.com>
2023-11-15 09:47:07 +0700

    Add embedded Calendly

--- blog_website/urls.py
+++ blog_website/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('', views.LatestPostsList.as_view(), name = 'home'),
     path('about/', views.About.as_view(), name = 'about'),    
     path('me/', views.About.as_view()),    
+    path('schedule/', views.Schedule, name = 'schedule'),    
     path('blog/', include('blog.urls')),    
     # path('summernote/', include('django_summernote.urls')),
     path('tinymce/', include('tinymce.urls')),
